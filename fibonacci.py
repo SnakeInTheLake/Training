cd0fd0985918ffaa809cee2771af351a79dbdf3f
SnakeInTheLake <44155497+SnakeInTheLake@users.noreply.github.com>
2019-01-20 15:53:16 +0300

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,7 @@
 def fib(n):  # Fibonacci number
     lst = [0, 1]
     for i in range(2, n+1):
-        lst.append((lst[i-1] + lst[i-2]) % 5)
+        lst.append(lst[i-1] + lst[i-2])
     return lst[n]
 
 
